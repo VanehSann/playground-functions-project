af4bffcf6972d85970f01908f6a585d3a7fc438e
VanehSann <vanessacbsantos22@gmail.com>
2021-12-03 10:43:25 -0300

    requisito 10 - feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,37 @@
 // Desafio 10
-function techList() {
+function techList(listaTech, name) {
   // seu código aqui
+let listaNova = [];
+let object = {};
+listaTech = listaTech.sort();
+if (listaTech.length === 0){
+  return "Vazio!";
+}
+
+  for(let i = 0; i < listaTech.length;i++){
+ object = {
+tech: listaTech[i],
+name: name};
+
+listaNova.push(object);
+
+}
+
+return listaNova;
 }
 
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
+  if (lista.length > 11) {
+    console.log("Array com tamanho incorreto.");
+   }
+   
+  for (let i = 0; i < lista.length; i++) {
+     if (lista[i] < 0 || lista[i] > 9) {
+      console.log("não é possível gerar um número de telefone com esses valores");
+     }
+   }
 }
 
 // Desafio 12
