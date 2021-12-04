299288c942867ede5bdf53d0bf78668aacfcd10e
VanehSann <vanessacbsantos22@gmail.com>
2021-12-04 01:27:03 -0300

    requisito bonus 11 - feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,18 +21,47 @@ return listaNova;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(lista) {
   // seu código aqui
-  if (lista.length > 11) {
-    console.log("Array com tamanho incorreto.");
+   //numero que vou usar pra validar numeros de vezes que aparece
+   let repeatNumber = 0;
+   // validar se é maior que ou menor que 11 digitos
+   if (lista.length > 11 || lista.length < 11 || lista.length === 0) {
+     return "Array com tamanho incorreto.";
    }
+ 
+ //verificando se é igual a 11 digitos
+  else {
+  let repeatNumber = 0;
+ for (let index = 0; index < lista.length; index++) {
    
-  for (let i = 0; i < lista.length; i++) {
-     if (lista[i] < 0 || lista[i] > 9) {
-      console.log("não é possível gerar um número de telefone com esses valores");
-     }
-   }
+ for (let i = 0; i < lista.length; i++) {
+   for (let j = i - 1; j < lista.length; j++) {
+  if (lista[j] === lista[i]) {
+    repeatNumber = repeatNumber + 0.20; 
+    
+    };}
+ } 
+ for (let indexDois = 0; indexDois < lista.length; indexDois++) {
+  if (lista[indexDois] < 0 || lista[indexDois] > 9 ) {
+    return "não é possível gerar um número de telefone com esses valores";
+    
+  }
+  
 }
+
+
+     if (lista[index] < 0 || lista[index] > 9 || repeatNumber >= 3) {
+       return "não é possível gerar um número de telefone com esses valores";
+       
+     }
+     else {
+       return `(${lista[0]}${lista[1]}) ${lista[2]}${lista[3]}${lista[4]}${lista[5]}${lista[6]}-${lista[7]}${lista[8]}${lista[9]}${lista[10]}`;
+     }
+       
+   }
+    }
+   } 
 
 // Desafio 12
 function triangleCheck() {
