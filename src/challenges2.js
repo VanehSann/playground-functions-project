59f94946b1e0094bf87761ea46fb5e7602405244
VanehSann <vanessacbsantos22@gmail.com>
2021-12-09 11:25:05 -0300

    me resta apenas um erro do lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,11 +18,8 @@ function techList(listaTech, nome) {
   return listaNova;
 }
 // Desafio 11
-function generatePhoneNumber(lista) {
-  // seu código aqui
-  if (lista.length > 11 || lista.length < 11 || lista.length === 0) {
-    return 'Array com tamanho incorreto.';
-  }
+// função de repetição de 3
+function repeatTimes3(lista) {
   let repeatNumber = 0;
   for (let i = 0; i < lista.length; i += 1) {
     for (let j = i - 1; j < lista.length; j += 1) {
@@ -31,18 +28,34 @@ function generatePhoneNumber(lista) {
       }
     }
   }
-  for (let indexDois = 0; indexDois < lista.length; indexDois += 1) {
-    if (lista[indexDois] < 0 || lista[indexDois] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
+  return repeatNumber;
+}
+// funcao de tamanho menor que zero e numero individual maior que 11
+function checando1(lista) {
+  let checks = 0;
+  for (let j = 0; j < lista.length; j += 1) {
+    if (lista[j] > 0) {
+      checks += 0.50;
+    }
+    if (lista[j] > 9) {
+      checks += 0.50;
     }
   }
-  for (let index = 0; index < lista.length; index += 1) {
-    if (lista[index] < 0 || lista[index] > 9 || repeatNumber >= 3) {
+  return checks;
+}
+// funcao principal
+function generatePhoneNumber(lista) {
+// seu código aqui
+  if (lista.length === 11) {
+    let repetion = repeatTimes3(lista);
+    let checkizinho = checando1(lista);
+    if (repetion >= 3 || checkizinho > 5 || checkizinho < 4.5) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
+    return `(${lista[0]}${lista[1]}) ${lista[2]}${lista[3]}${lista[4]}${lista[5]}${lista[6]}-`
+      + `${lista[7]}${lista[8]}${lista[9]}${lista[10]}`;
   }
-  return `(${lista[0]}${lista[1]}) ${lista[2]}${lista[3]}${lista[4]}${lista[5]}${lista[6]}-`
-    + `${lista[7]}${lista[8]}${lista[9]}${lista[10]}`;
+  return 'Array com tamanho incorreto.';
 }
 // let lista = [1, 2, 3, 4, 5, 6, 9, 9, 9, 0, -1];
 // console.log(generatePhoneNumber(lista))
